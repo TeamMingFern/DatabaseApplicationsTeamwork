69c98a0ed92a9ba6bd3194e02f661a22f1cd0df7
MKURSHUMOV <kur6uma666@gmail.com>
2015-07-25 16:22:27 +0300

    Initial commit for MongoDB problem

---
+++ _05_Problem_MongoDB/JSON_ReportsInMongoDB.cs
@@ -0,0 +1,26 @@
+﻿namespace MongoDB
+{
+    using Bson;
+    using Bson.Serialization;
+    using Driver;
+
+    public static class JSON_ReportsInMongoDB
+    {
+        private const string connectionString = "mongodb://localhost:27017/supermarket";
+        private static readonly MongoClient client = new MongoClient(connectionString);
+        private static readonly IMongoDatabase database = client.GetDatabase("supermarket");
+
+        private static readonly IMongoCollection<BsonDocument> collection =
+            database.GetCollection<BsonDocument>("SalesByProductReports");
+
+        public static async void ImportSalesByProductReport(string jsonReport)
+        {
+            await collection.InsertOneAsync(BsonSerializer.Deserialize<BsonDocument>(jsonReport));
+        }
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
